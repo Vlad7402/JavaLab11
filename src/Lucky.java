f150ced066406494d6f72bbb779202bd23cf7c2f
vlaD <vlad.74022@gmail.com>
2023-01-17 03:59:12 +0500

    Task 01 done

--- src/Lucky.java
+++ src/Lucky.java
@@ -1,43 +1,35 @@
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Lucky {
 
-    public static void main(String[] args) throws InterruptedException {
+    static AtomicInteger x = new AtomicInteger(0);
+    static AtomicInteger count = new AtomicInteger(0);
 
-        // создаем пул для выполнения наших задач
-        //   максимальное количество созданных задач - 3
-        ThreadPoolExecutor executor = new ThreadPoolExecutor(
-                // не изменяйте эти параметры
-                3, 3, 1, TimeUnit.SECONDS, new LinkedBlockingQueue<>(3));
-
-        // сколько задач выполнилось
-        AtomicInteger count = new AtomicInteger(0);
-
-        // сколько задач выполняется
-        AtomicInteger inProgress = new AtomicInteger(0);
-
-        // отправляем задачи на выполнение
-        for (int i = 0; i < 30; i++) {
-            final int number = i;
-            Thread.sleep(10);
-
-            System.out.println("creating #" + number);
-            executor.submit(() -> {
-                int working = inProgress.incrementAndGet();
-                System.out.println("start #" + number + ", in progress: " + working);
-                try {
-                    // тут какая-то полезная работа
-                    Thread.sleep(Math.round(1000 + Math.random() * 2000));
-                } catch (InterruptedException e) {
-                    // ignore
+    static class LuckyThread extends Thread {
+        @Override
+        public void run() {
+            while (x.get() < 999999) {
+                int x = Lucky.x.incrementAndGet();
+                if ((x % 10) + (x / 10) % 10 + (x / 100) % 10 == (x / 1000)
+                        % 10 + (x / 10000) % 10 + (x / 100000) % 10) {
+                    System.out.println(x);
+                    count.incrementAndGet();
                 }
-                working = inProgress.decrementAndGet();
-                System.out.println("end #" + number + ", in progress: " + working + ", done tasks: " + count.incrementAndGet());
-                return null;
-            });
+            }
+
         }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new LuckyThread();
+        Thread t2 = new LuckyThread();
+        Thread t3 = new LuckyThread();
+        t1.start();
+        t2.start();
+        t3.start();
+        t1.join();
+        t2.join();
+        t3.join();
+        System.out.println("Total: " + count);
     }
 }
